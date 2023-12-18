56656c45c6797ea5bba339330a5201594f6217e1
spectre-xenon <62655598+spectre-xenon@users.noreply.github.com>
2023-12-18 14:28:14 +0000

    test: added ship factory test

---
+++ tests/ship.test.js
@@ -0,0 +1,19 @@
+import { test, expect } from "vitest";
+import Ship from "../src/lib/ship";
+
+const myShip = Ship(4);
+
+test("ship hit() function", () => {
+  expect(myShip.hit()).toBe(1);
+});
+
+test("ship not sunk yet from 2 hits", () => {
+  expect(myShip.isSunk()).toBe(false);
+});
+
+test("ship sinks after 4 hits", () => {
+  myShip.hit();
+  myShip.hit();
+  myShip.hit();
+  expect(myShip.isSunk()).toBe(true);
+});
