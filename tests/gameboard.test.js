489b7f3b8abb751a0976d56183ff59fa20012860
spectre-xenon <62655598+spectre-xenon@users.noreply.github.com>
2023-12-20 21:13:49 +0000

    test: added tests for gameboard placing logic

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -30,6 +30,44 @@ describe("Placing tests", () => {
     expect(myGameBoard.place(12, 3)).toBe(false);
   });
 
+  test("handles placing ships next to other ships", () => {
+    expect(myGameBoard.place(12, 3, "x")).toBe(true);
+    expect(myGameBoard.place(16, 3, "y")).toBe(true);
+
+    // Right x
+    expect(myGameBoard.place(15, 3, "x")).toBe(false);
+    // Left x
+    expect(myGameBoard.place(11, 3, "x")).toBe(false);
+    // Bellow x
+    expect(myGameBoard.place(21, 3, "x")).toBe(false);
+    expect(myGameBoard.place(22, 3, "x")).toBe(false);
+    expect(myGameBoard.place(23, 3, "x")).toBe(false);
+    expect(myGameBoard.place(24, 3, "x")).toBe(false);
+    expect(myGameBoard.place(25, 3, "x")).toBe(false);
+    // Above x
+    expect(myGameBoard.place(1, 3, "x")).toBe(false);
+    expect(myGameBoard.place(2, 3, "x")).toBe(false);
+    expect(myGameBoard.place(3, 3, "x")).toBe(false);
+    expect(myGameBoard.place(4, 3, "x")).toBe(false);
+    expect(myGameBoard.place(5, 3, "x")).toBe(false);
+    // Right y
+    expect(myGameBoard.place(7, 3, "y")).toBe(false);
+    expect(myGameBoard.place(17, 3, "y")).toBe(false);
+    expect(myGameBoard.place(27, 3, "y")).toBe(false);
+    expect(myGameBoard.place(37, 3, "y")).toBe(false);
+    expect(myGameBoard.place(47, 3, "y")).toBe(false);
+    // Left y
+    expect(myGameBoard.place(5, 3, "y")).toBe(false);
+    expect(myGameBoard.place(15, 3, "y")).toBe(false);
+    expect(myGameBoard.place(25, 3, "y")).toBe(false);
+    expect(myGameBoard.place(35, 3, "y")).toBe(false);
+    expect(myGameBoard.place(45, 3, "y")).toBe(false);
+    // Bellow y
+    expect(myGameBoard.place(46, 3, "y")).toBe(false);
+    // Above y
+    expect(myGameBoard.place(6, 3, "y")).toBe(false);
+  });
+
   test("handles intersecting ships", () => {
     myGameBoard.place(11, 3);
     expect(myGameBoard.place(1, 3, "y")).toBe(false);
