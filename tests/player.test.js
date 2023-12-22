c9da57a2e0986f9e951312cfda67c5df291ce26b
spectre-xenon <62655598+spectre-xenon@users.noreply.github.com>
2023-12-22 20:28:10 +0000

    test: added player tests

---
+++ tests/player.test.js
@@ -0,0 +1,22 @@
+import { describe, test, expect } from "vitest";
+import { Player, Computer } from "../src/factories/player";
+import { beforeEach } from "vitest";
+
+describe("Player tests", () => {
+  let player;
+  let enemy;
+
+  beforeEach(() => {
+    player = Player();
+    enemy = Computer();
+  });
+
+  test("Player attacks enemy ships", () => {
+    enemy.board.place(1, 3);
+    expect(player.attack(enemy.board, 2)).toBe("hit");
+  });
+
+  test("Player attacks empty block", () => {
+    expect(player.attack(enemy.board, 2)).toBe("miss");
+  });
+});
